ff37dc9412b625825eda7398569aa48767a29c31
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-10-02 17:34:14 -0500

    creatd HTML structure, created variables, question array, timer, reset function

---
+++ assets/javascript/app.js
@@ -0,0 +1,107 @@
+// Questions
+var questions = [
+    {
+        text: "What color is the sky?",
+        options:["red", "blue", "purple"],
+        correctIndex: 1,
+    },
+    {
+        text: "What color is the sky?",
+        options:["red", "blue", "purple"],
+        correctIndex: 2,
+    },
+    {
+        text: "What color is the sky?",
+        options:["red", "blue", "purple"],
+        correctIndex: 0,
+    }
+]
+
+// Global Variables
+var timer = 0;
+var totalGameTime = 5;
+var isTimerRunning = false;
+
+//Display Instructions and Button
+function preGame() {
+    var instructionsContent = "blah blah instructions";
+    $(".instructions").text(instructionsContent);
+    $(".timercontainer").hide();
+    $(".questioncontainer").hide();
+    $(".results").hide();
+    $(".startbutton").click(function () {
+        $(".instructions").hide();
+        $(this).hide();
+        startGame();
+    })
+}
+
+
+// Start Game function
+function startGame() {
+    $(".timercontainer").show();
+    $(".questioncontainer").show();
+    // Start Timer
+    startTimer();
+    // Display questions
+    displayQuestions();
+    // Display options
+    // Include Submit button
+    // Click submit
+    // end game
+
+}
+
+function displayQuestions() {
+
+}
+
+function startTimer(isTimerRunning) {
+    if (!isTimerRunning) {
+        isTimerRunning = true;
+        timer = setInterval(function () {
+            $(".timercountdown").text(totalGameTime);
+            totalGameTime--;
+            if (totalGameTime < 0) {
+                isTimerRunning = false;
+                clearInterval(timer);
+                totalGameTime = 5;
+                endGame();
+            }
+        }, 1000);
+    }
+
+}
+
+function resetTimer() {
+
+}
+
+// End Game function
+function endGame() {
+    $(".timercontainer").hide();
+    $(".questioncontainer").hide();
+    $(".results").show();
+
+    // Display correct, incorrect, unanswered 
+
+    // Restart game (try again), same as start game? call start game function again
+    $(".restartbutton").click(function () {
+        
+        $(".instructions").show();
+        $(".startbutton").show();
+        preGame();
+    })
+}
+
+
+
+
+
+
+$(document).ready(function () {
+    preGame();
+
+
+    console.log("this");
+});
